e04660150bd317d59a45d6ba39bd64e1ade4f422
MarcDagher <marcdagher47@gmail.com>
2023-12-07 18:24:21 +0200

    add join to read

--- read_products.php
+++ read_products.php
@@ -7,7 +7,8 @@ $user_id = isset($_POST['user_id']) && !empty($_POST['user_id']) ? $_POST['user_
 
 
 $arr = [];
-$get_data = $mysqli -> prepare("select product_name, price, stock, user_id from products");
+$get_data = $mysqli->prepare("SELECT product_name, price, username FROM products JOIN users ON products.user_id = users.user_id");
+
 $get_data -> execute();
 $results = $get_data -> get_result();
 
@@ -16,17 +17,6 @@ while ($arr_results = $results -> fetch_assoc()){
 }
 $get_data -> close();
 
-$data = [];
-foreach ($arr as $key){
-  $user = $mysqli -> prepare("select username from users where user_id = ?");
-  $user -> bind_param('i', $key['user_id']);
-  $user -> execute();
-  $user -> store_result();
-  $user -> bind_result($username);
-  $user -> fetch();
-  
-  $key['user_id'] = $username;
-  $user -> close();
-  $data[] = $key;
+foreach($arr as $info){
+  echo json_encode($info) , "\n";
 }
-echo json_encode($data);
